dad3df769273e3b820a02369f5889ef913c361ee
m lutfi <mlutfiibra@outlook.com>
2017-11-27 00:36:12 +0700

    Add Codehacking project

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,14 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::auth();
+
+Route::get('/home', 'HomeController@index');
+
+Route::get('/admin', function (){
+    return view('admin.index');
+});
+
+
+Route::resource('admin/users', 'AdminUsersController');
---
+++ resources/views/admin/users/index.blade.php
@@ -0,0 +1,45 @@
+@extends('layouts.admin')
+
+
+@section('content')
+    <h1>Users</h1>
+
+    <table class="table">
+        <thead>
+        <tr>
+            <th>ID</th>
+            <th>Photo</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Status</th>
+            <th>Created</th>
+            <th>Update</th>
+            <th>Role</th>
+        </tr>
+        </thead>
+        <tbody>
+
+        @if($users)
+            @foreach($users as $user)
+                <tr>
+                    <td>{{$user->id}}</td>
+
+                    {{--@if($user->photo)--}}
+                        {{----}}
+                    {{--@endif--}}
+
+                    <td><img height="50px" src="{{$user->photo ? $user->photo->file : 'http://placehold.it/50x50'}}" alt=""></td>
+                    <td><a href="{{route('admin.users.edit', $user->id)}}">{{$user->name}}</a></td>
+                    <td>{{$user->email}}</td>
+                    <td>{{$user->is_active == 1 ? "Active" : "Not Active"}}</td>
+                    <td>{{$user->created_at->diffForHumans()}}</td>
+                    <td>{{$user->updated_at->diffForHumans()}}</td>
+                    <td>{{$user->role->name}}</td>
+                </tr>
+            @endforeach
+        @endif
+
+        </tbody>
+    </table>
+
+@stop
